8c4092b47edb6955ea9e82e4fafe3949ea81015a
dglshenriq <dglsramone@gmail.com>
2026-08-12 18:43:15 -0300

    Adiciona checkbox musica e inicia temporizador

--- scripts.js
+++ scripts.js
@@ -4,11 +4,30 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const startPauseBt = document.querySelector('#start-pause');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
 
 function alteraContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagens/${contexto}.png`);
-
+    botoes.forEach((botao) => {
+        botao.classList.remove('active');
+    });
     switch (contexto) {
         case 'foco':
             titulo.innerHTML = `Otimize sua produtividade,<br />
@@ -29,12 +48,40 @@ function alteraContexto(contexto) {
 
 focoBt.addEventListener('click', () => {
     alteraContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
     alteraContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
     alteraContexto('descanso-longo');
+    longoBt.classList.add('active');
 });
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+       zerar();
+       alerta('Tempo finalizado!');
+       return;
+    }
+   tempoDecorridoEmSegundos-= 1;
+   console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function inciarOuPausar () {   
+    if (intervaloId) {
+        zerar();
+        return;
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar(){
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
