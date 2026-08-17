c577aec286261e1ed836b6f10711d33149fe1c9c
dglshenriq <dglsramone@gmail.com>
2026-08-17 18:10:18 -0300

    Inicia script textarea

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,17 @@
 const bntAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+const tarefas = []
 
 bntAdicionarTarefa.addEventListener('click', () =>{
     formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
