1e25ff42e72cf0cb2276bd2e43e608c1b5e776e6
dglshenriq <dglsramone@gmail.com>
2026-08-19 15:46:00 -0300

    Adiciona limpar tarefas concluídas

--- script-crud.js
+++ script-crud.js
@@ -4,7 +4,9 @@ const textArea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 const paragrafoDescricaoTarefas = document.querySelector('.app__section-active-task-description')
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
 let liTarefaSelecionada = null
 
@@ -108,3 +110,12 @@ document.addEventListener('FocoFinalizado', () => {
         atualizarTarefas()
     }
 }) 
+
+btnRemoverConcluidas.onclick = () => {
+    const seletor = ".app__section-task-list-item-complete"
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    })
+    tarefas = tarefas.filter(tarefa => !tarefa.completa)
+    atualizarTarefas()
+}
