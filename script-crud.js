97be8c5b99d6fd87963aa4d186c655678d16c881
dglshenriq <dglsramone@gmail.com>
2026-08-19 15:56:48 -0300

    Adiciona limpar todas as tarefas

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,8 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 const paragrafoDescricaoTarefas = document.querySelector('.app__section-active-task-description')
 
 const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+const btnRemoverTodas = document.querySelector('#btn-remover-todas')
+
 
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
@@ -111,11 +113,19 @@ document.addEventListener('FocoFinalizado', () => {
     }
 }) 
 
-btnRemoverConcluidas.onclick = () => {
-    const seletor = ".app__section-task-list-item-complete"
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item"
     document.querySelectorAll(seletor).forEach(elemento => {
         elemento.remove()
     })
-    tarefas = tarefas.filter(tarefa => !tarefa.completa)
+    tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : []
     atualizarTarefas()
+}
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true)
+
+btnRemoverTodas.onclick = () => {
+    if (confirm('Tem certeza que deseja remover todas as tarefas?')) {
+        removerTarefas(false)
+    }
 }
